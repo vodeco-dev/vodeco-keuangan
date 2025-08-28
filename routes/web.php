a039be1b260d6f5dcee5d43416289f74342b2bcf
ademas-w <wahyusiakrom238@gmail.com>
2025-08-28 13:55:59 +0700

    penambahan transactions

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/transactions', [TransactionsController::class, 'index'])->name('transactions');
+
+require __DIR__ . '/auth.php';
