853cd2adfd76d5b71bbf36377845289ea32a16df
ademas-w <wahyusiakrom238@gmail.com>
2025-08-28 14:15:38 +0700

    fix menu route

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,26 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DebtController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\SettingController;
+use App\Http\Controllers\TransactionController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-Route::get('/transactions', [TransactionsController::class, 'index'])->name('transactions');
-
-Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
+// Menggunakan middleware 'auth' untuk memastikan hanya user yang sudah login
+// yang bisa mengakses halaman-halaman ini.
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions');
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
+    Route::get('/reports', [ReportController::class, 'index'])->name('reports');
+    Route::get('/debts', [DebtController::class, 'index'])->name('debts');
+    Route::get('/settings', [SettingController::class, 'index'])->name('settings');
+});
 
 require __DIR__ . '/auth.php';
