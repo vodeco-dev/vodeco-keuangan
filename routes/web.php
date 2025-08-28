3e0a8cf4a44949804eaf19dfa20f49652aa8fe61
ademas-w <wahyusiakrom238@gmail.com>
2025-08-28 13:39:51 +0700

    perbaikan rout

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,7 @@
 use App\Http\Controllers\DashboardController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
