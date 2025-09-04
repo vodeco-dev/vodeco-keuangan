77147a5a273e3b4ba24d633131ad3fa69cf1402e
ademaswahyu <53292337+ademas-wahyu@users.noreply.github.com>
2025-09-04 13:35:11 +0700

    feat: add service cost navigation

--- app/Providers/NavigationServiceProvider.php
+++ app/Providers/NavigationServiceProvider.php
@@ -37,6 +37,12 @@ class NavigationServiceProvider extends ServiceProvider
                 'icon' => '<svg fill="currentColor" height="24px" viewBox="0 0 256 256" width="24px" xmlns="http://www.w3.org/2000/svg"><path d="M243.31,136,144,36.69A15.86,15.86,0,0,0,132.69,32H40a8,8,0,0,0-8,8v92.69A15.86,15.86,0,0,0,36.69,144L136,243.31a16,16,0,0,0,22.63,0l84.68-84.68a16,16,0,0,0,0-22.63Zm-96,96L48,132.69V48h84.69L232,147.31ZM96,84A12,12,0,1,1,84,72,12,12,0,0,1,96,84Z"></path></svg>',
             ],
             [
+                'name' => 'Service Cost',
+                'route' => 'service_costs.index',
+                'icon' => '<svg fill="currentColor" height="24px" viewBox="0 0 256 256" width="24px" xmlns="http://www.w3.org/2000/svg"><path d="M216 72H40a16 16 0 0 0-16 16v80a16 16 0 0 0 16 16h176a16 16 0 0 0 16-16V88a16 16 0 0 0-16-16Zm0 96H40V88h176v80Zm-40-40a16 16 0 1 1-16-16 16 16 0 0 1 16 16Z"></path></svg>',
+                'can' => 'admin',
+            ],
+            [
                 'name' => 'Hutang',
                 'route' => 'debts.index',
                 'icon' => '<svg fill="currentColor" height="24px" viewBox="0 0 256 256" width="24px" xmlns="http://www.w3.org/2000/svg"><path d="M224,48H32A16,16,0,0,0,16,64V192a16,16,0,0,0,16,16H224a16,16,0,0,0,16-16V64A16,16,0,0,0,224,48Zm0,16V88H32V64Zm0,128H32V104H224v88Zm-16-24a8,8,0,0,1-8,8H168a8,8,0,0,1,0-16h32A8,8,0,0,1,208,168Zm-64,0a8,8,0,0,1-8,8H120a8,8,0,0,1,0-16h16A8,8,0,0,1,144,168Z"></path></svg>',
