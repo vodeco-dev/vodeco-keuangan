b27ea7b7b7e1b03cc746583de2eaf9716c103c54
ademaswahyu <53292337+ademas-wahyu@users.noreply.github.com>
2025-09-06 09:17:29 +0700

    Add service cost seeder and usage docs

---
+++ database/seeders/ServiceCostSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\ServiceCost;
+
+class ServiceCostSeeder extends Seeder
+{
+    /**
+     * Seed the service_costs table with default types.
+     */
+    public function run(): void
+    {
+        $serviceCosts = [
+            ['id' => ServiceCost::PASS_THROUGH_ID, 'name' => 'Pass-Through'],
+            ['id' => ServiceCost::DOWN_PAYMENT_ID, 'name' => 'Down Payment'],
+            ['id' => 3, 'name' => 'Agency Fee'],
+        ];
+
+        ServiceCost::upsert($serviceCosts, ['id'], ['name']);
+    }
+}
