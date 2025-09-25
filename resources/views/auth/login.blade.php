1f002248d7d0afa0dec6e03f13d5ddc08bf6e03d
ademas-wahyu <wahyusiakrom238@gmail.com>
2025-09-25 09:16:58 +0700

    editing login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,8 @@
 <x-guest-layout>
     <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+        <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <!-- Email Address -->
