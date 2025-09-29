1ac2ea43e09d98f029451607de98b99e9311c5df
ademas-wahyu <wahyusiakrom238@gmail.com>
2025-09-29 11:57:01 +0700

    re fix checklist

--- resources/views/debts/index.blade.php
+++ resources/views/debts/index.blade.php
@@ -76,8 +76,9 @@
                 <span>Tambah Catatan Baru</span>
             </button>
             <button @click="categoryModal = true" type="button" class="p-2 bg-white border border-gray-200 rounded-lg text-gray-600 hover:bg-gray-100" title="Pengaturan pilihan kategori">
-                <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.983 5.5a1.5 1.5 0 102.828-1.06l.53-.53a9.04 9.04 0 012.75 1.59l-.53.53a1.5 1.5 0 101.06 2.828l.53.53a9.04 9.04 0 011.59 2.75l-.53.53a1.5 1.5 0 10-1.06 2.828l.53.53a9.04 9.04 0 01-1.59 2.75l-.53-.53a1.5 1.5 0 10-2.828 1.06l-.53.53a9.04 9.04 0 01-2.75-1.59l.53-.53a1.5 1.5 0 10-1.06-2.828l-.53-.53a9.04 9.04 0 01-1.59-2.75l.53-.53a1.5 1.5 0 101.06-2.828l-.53-.53a9.04 9.04 0 011.59-2.75l.53.53z"></path>
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13v-2a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L14 4.757V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L4.929 6.343a1 1 0 0 0 0 1.414l.536.536L4.757 10H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535 1.707.707V20a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H20a1 1 0 0 0 1-1Z"/>
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
                 </svg>
             </button>
         </div>
