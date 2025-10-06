582934f6e71cce853977d69b57c78e5007cf4db0
ademaswahyu <53292337+ademas-wahyu@users.noreply.github.com>
2025-10-06 09:28:03 +0700

    Add PDF watermark using centered image

--- resources/views/exports/financial_report_pdf.blade.php
+++ resources/views/exports/financial_report_pdf.blade.php
@@ -1,3 +1,11 @@
+@php
+    $watermarkPath = public_path('image2.png');
+    $watermarkExtension = strtolower(pathinfo($watermarkPath, PATHINFO_EXTENSION));
+    if ($watermarkExtension === 'jpg') {
+        $watermarkExtension = 'jpeg';
+    }
+    $watermarkData = is_file($watermarkPath) ? base64_encode(file_get_contents($watermarkPath)) : null;
+@endphp
 <!DOCTYPE html>
 <html lang="id">
 <head>
@@ -7,8 +15,25 @@
     <style>
         body {
             padding: 32px 40px;
-            background: url("public/image2.png") no-repeat center;
+            position: relative;
         }
+
+        @if ($watermarkData)
+        body::before {
+            content: "";
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 100%;
+            height: 100%;
+            background-image: url('data:image/{{ $watermarkExtension }};base64,{{ $watermarkData }}');
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: contain;
+            opacity: 0.08;
+        }
+        @endif
 
         .report-wrapper {
             width: 100%;
