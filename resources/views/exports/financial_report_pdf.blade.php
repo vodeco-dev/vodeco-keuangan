3606c41c97c04440439f8315b2db35cb8ffd408f
ademas-wahyu <wahyusiakrom238@gmail.com>
2025-10-06 09:49:46 +0700

    fix ratio

--- resources/views/exports/financial_report_pdf.blade.php
+++ resources/views/exports/financial_report_pdf.blade.php
@@ -24,7 +24,6 @@
             left: 50%;
             transform: translate(-50%, -50%);
             width: 100%;
-            height: 100%;
             opacity: 0.08;
             z-index: 0;
             pointer-events: none;
